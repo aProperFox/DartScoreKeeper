d39ee1fe2c1b9f2d7d270e50ea81bbdce24f387e
olsontl <olsontl@colorado.edu>
2014-09-03 22:40:05 -0600

    initial commit

---
+++ app/src/androidTest/java/games/ride/car/dartscorekeeper/ApplicationTest.java
@@ -0,0 +1,13 @@
+package games.ride.car.dartscorekeeper;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
